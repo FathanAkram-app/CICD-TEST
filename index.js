45b63bd32bc66f05baa4694ff41c8a448c2bf0d6
fathan <fathan.a.dev@gmail.com>
2022-04-10 13:49:35 +0700

    nodejs workflows

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const app = express()
+
+const bodyParser = require('body-parser')
+
+const http = require('http');
+const server = http.createServer(app);
+const port = 3000
+
+app.use(express.static(__dirname+'/public'));
+
+
+app.use(bodyParser.json())
+
+app.get('/',(_,res)=>{
+    res.send({
+        status: "success",
+        status_code:200
+    })
+})
+
+server.listen(port, () => {
+  console.log(`🚀 listening on port ${port} 🚀`)
+})
